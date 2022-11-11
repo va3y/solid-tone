f8a102f3176315ef1f1bc75b06a70519277d4645
Валерий Иванов <va.ivanov@cian.ru>
2022-11-11 18:30:05 +0400

    get around chrome permissions

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import * as Tone from "tone";
-import { createSignal, onMount } from "solid-js";
+import { createSignal, onMount, Show } from "solid-js";
 import { Synth } from "tone";
 
 export default function Home() {
 	const [synthBase, setSynth] = createSignal<Synth>();
 	const [sawSynth, setSawSynth] = createSignal<Synth>();
+	const [showMagicDiv, setShowMagicDiv] = createSignal(false);
 
 	const onPress = () => {
+		setShowMagicDiv(true);
 		const synth = synthBase().toDestination();
 		const now = Tone.now();
 		synth.triggerAttackRelease("C4", "8n", now);
@@ -43,13 +45,16 @@ export default function Home() {
 
 	return (
 		<main class='text-center mx-auto text-gray-700 p-4'>
-			<button onClick={onPress}>press me </button>
-			<h1 class='max-6-xs text-6xl text-sky-700 font-thin uppercase my-16'>
-				Hello world!
-			</h1>
-			<div class='mx-auto max-w-lg h-96 bg-sky-100' onMouseMove={onMouseMove}>
-				magic div
-			</div>
+			<button onClick={onPress}>
+				<h1 class='max-6-xs text-6xl text-sky-700 font-thin uppercase my-16'>
+					Hello world! (press me)
+				</h1>
+			</button>
+			<Show when={showMagicDiv()}>
+				<div class='mx-auto max-w-lg h-96 bg-sky-100' onMouseMove={onMouseMove}>
+					magic div
+				</div>
+			</Show>
 		</main>
 	);
 }
